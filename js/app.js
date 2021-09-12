8806e8093906bf3363638857c5b1c08ef0bf0fda
Xelem <69058100+Xelem@users.noreply.github.com>
2021-09-12 21:40:47 +0100

    Added my own JS solution

--- js/app.js
+++ js/app.js
@@ -4,7 +4,30 @@ const storeItems = document.querySelectorAll('.store-item');
 buttons.forEach(function(button) {
     button.addEventListener('click', (filterAction) => {
         filterAction.preventDefault();
+
+        // WRONG CODE
         
+        // if (button.textContent === 'all'){
+        //     storeItems.forEach(function(storeItem) {
+        //         storeItem.style.display = 'block';
+        //     })
+        // } else if (button.textContent === 'cakes'){
+        //     storeItems.forEach(function(storeItem) {
+        //         storeItem.style.display = 'block';
+        //     }) 
+        // } else if (button.textContent === 'cupcakes'){
+        //     storeItems.forEach(function(storeItem) {
+        //         storeItem.style.display = 'block';
+        //     })
+        // }  else if (button.textContent === 'sweets'){
+        //     storeItems.forEach(function(storeItem) {
+        //         storeItem.style.display = 'block';
+        //     })
+        // }  else if (button.textContent === 'doughnuts'){
+        //     storeItems.forEach(function(storeItem) {
+        //         storeItem.style.display = 'block';
+        //     })
+        // }
 
     })
 })
