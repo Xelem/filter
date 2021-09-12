6153dd6b9ce12490dfa08690e94d2abad3fdd8ed
Xelem <69058100+Xelem@users.noreply.github.com>
2021-09-12 21:03:43 +0100

    The filter project

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const buttons = document.querySelectorAll('.btn');
+const storeItems = document.querySelectorAll('.store-item');
+
+buttons.forEach(function(button) {
+    button.addEventListener('click', (filterAction) => {
+        filterAction.preventDefault();
+        
+
+    })
+})
