6f840c53d9e3437326ae23c39c8957de66906302
Xelem <69058100+Xelem@users.noreply.github.com>
2021-09-23 16:22:38 +0100

    Working Code

--- js/app.js
+++ js/app.js
@@ -1,33 +1,63 @@
 const buttons = document.querySelectorAll('.btn');
 const storeItems = document.querySelectorAll('.store-item');
+const searchField = document.querySelector('#search-item');
 
+
+// FILTER
 buttons.forEach(function(button) {
     button.addEventListener('click', (filterAction) => {
         filterAction.preventDefault();
 
-        // WRONG CODE
-        
-        // if (button.textContent === 'all'){
-        //     storeItems.forEach(function(storeItem) {
-        //         storeItem.style.display = 'block';
-        //     })
-        // } else if (button.textContent === 'cakes'){
-        //     storeItems.forEach(function(storeItem) {
-        //         storeItem.style.display = 'block';
-        //     }) 
-        // } else if (button.textContent === 'cupcakes'){
-        //     storeItems.forEach(function(storeItem) {
-        //         storeItem.style.display = 'block';
-        //     })
-        // }  else if (button.textContent === 'sweets'){
-        //     storeItems.forEach(function(storeItem) {
-        //         storeItem.style.display = 'block';
-        //     })
-        // }  else if (button.textContent === 'doughnuts'){
-        //     storeItems.forEach(function(storeItem) {
-        //         storeItem.style.display = 'block';
-        //     })
-        // }
 
+        if (button.textContent === 'all'){
+            storeItems.forEach(function(storeItem) {
+                storeItem.style.display = 'block';
+            })
+        } else if (button.textContent === 'cakes'){
+            storeItems.forEach(function(storeItem) {
+                if (storeItem.dataset.item === 'cakes'){
+                    storeItem.style.display = 'block';
+                } else {
+                    storeItem.style.display = 'none';
+                }   
+            }) 
+        } else if (button.textContent === 'cupcakes'){
+            storeItems.forEach(function(storeItem) {
+                if (storeItem.dataset.item === 'cupcakes'){
+                    storeItem.style.display = 'block';
+                } else {
+                    storeItem.style.display = 'none';
+                };
+            })
+        }  else if (button.textContent === 'sweets'){
+            storeItems.forEach(function(storeItem) {
+                if (storeItem.dataset.item === 'sweets'){
+                    storeItem.style.display = 'block';
+                } else {
+                    storeItem.style.display = 'none';
+                }
+            })
+        }  else if (button.textContent === 'doughnuts'){
+            storeItems.forEach(function(storeItem) {
+                if (storeItem.dataset.item === 'doughnuts'){
+                    storeItem.style.display = 'block';
+                } else {
+                    storeItem.style.display = 'none';
+                }
+            })
+        }
+
+    })
+})
+
+// SEARCH
+searchField.addEventListener('input', () => {
+    const searchText = searchField.value;
+    storeItems.forEach(function(storeItem) {
+        if (storeItem.dataset.item.indexOf(searchText) > -1){
+            storeItem.style.display = 'block';
+        } else {
+            storeItem.style.display = 'none';
+        }   
     })
 })
